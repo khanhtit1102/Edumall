f89c17cc5b0bbcff0262cfc1a4f566788dda9182
Nong Van Khanh <khanhnongvan0@gmail.com>
2020-05-27 13:22:46 +0700

    Fix conflict & email

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ date_default_timezone_set('Asia/Ho_Chi_Minh');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://luongvantri.htc-it.team';
+$config['base_url'] = 'http://[::1]/Edumall';
 
 /*
 |--------------------------------------------------------------------------
